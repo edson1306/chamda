59d41b1c0165e88a45447b029611b6853dd15cd6
edson1306 <129901914+edson1306@users.noreply.github.com>
2024-06-13 12:22:19 -0300

    Add files via upload

---
+++ chamada.js
@@ -0,0 +1,4 @@
+const alunos = ["Ana Carolina","Ana Heloise","Ana Luiza","Andre","Beatriz","Bhianca","Caua","Elia","Enzo","Giovanna","Henrique Duarte","Henrique Emilio","Iago","Iasmin","Isadora","Jhenyfer","Joao Francisco","Joao Henrique","Lorenzo","Luiz Felipe","Luiz Gustavo","Maria julia","Maria gabriela","paola","Pedro","Renato","Richard","Victor","Vinicius","Willan","Thaissa"];
+const numero = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,2006,25,26,27,28,29,30,31,32,33]
+
+const listaDeChamada
